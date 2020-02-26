5e90a70a040ea8002ef6d2fd35e4f90629fb0675
rainer.telesko <rainer.telesko@fhnw.ch>
2020-02-26 10:21:47 +0100

    Fourth commit

--- app/src/main/java/com/example/lotterylisttwo/MainActivity.java
+++ app/src/main/java/com/example/lotterylisttwo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lotterylisttwo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -7,7 +8,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
         // Get reference to the ListView
         lvTips = findViewById(R.id.lvTips);
 
-        // Initializing a new String Array
-        String[] tips = new String[]{"Tip 1", "Tip 2", "Tip 3", "Tip 4", "Tip 5", "Tip 6", "Tip 7", "Tip 8", "Tip 9", "Tip 10",
-        };
-
+        // Initializing a new ArrayList
+        final ArrayList<String> tips = new ArrayList<>();
+        tips.add("Tip 1");
+        tips.add("Tip 2");
+        tips.add("Tip 3");
+        tips.add("Tip 4");
+        tips.add("Tip 5");
+        tips.add("Tip 6");
+        tips.add("Tip 7");
+        tips.add("Tip 8");
+        tips.add("Tip 9");
+        tips.add("Tip 10");
         /*
         What are possible Adapters ind Android?
         BaseAdapter is a very generic adapter that allows you to do pretty much whatever you want.
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         */
 
         // Create an ArrayAdapter from the List
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, tips);
 
         // Populate the ListView with items from ArrayAdapter
@@ -44,12 +56,40 @@ public class MainActivity extends AppCompatActivity {
         // Set an item click Listener for ListView
         lvTips.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                /* Alternative 1: Just show a Toast message
                 // Get the selected item text from the ListView
                 String selectedItem = (String) parent.getItemAtPosition(position);
 
                 // Display the selected item text with a Toast message
                 Toast.makeText(getApplicationContext(), selectedItem + " selected", Toast.LENGTH_LONG).show();
+                */
+
+                // Alternative 2: Delete an item with an AlertDialog
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setMessage("Are you sure you want to delete this item?")
+                        .setTitle("Attempt to delete an item")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                try {
+                                    tips.remove(position);
+                                    arrayAdapter.notifyDataSetChanged();
+
+                                    Toast.makeText(MainActivity.this, "Item deleted!", Toast.LENGTH_SHORT).show();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "Item NOT deleted!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
             }
         });
     }
